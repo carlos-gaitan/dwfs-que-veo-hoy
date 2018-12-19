1c460ea0853a7ceef2a83e1980b0357a61e91ab3
elcharly <ninguno@elcharly.com>
2018-12-18 22:37:46 -0300

    Gracias Juan :)

--- servidor/controladores/peliculasController.js
+++ servidor/controladores/peliculasController.js
@@ -1,12 +1,20 @@
 var Peliculas = require('../modelos/peliculas');
+// http://localhost:8080/peliculas?pagina=1&titulo=pepe&genero=4&anio=1981&cantidad=52&columna_orden=titulo&tipo_orden=ASC
 
 function obtenerPeliculas(req, res) {
-  Peliculas.obtenerTodos(function(error, resultadoQuery){
+  Peliculas.obtenerTodos(req.query, function(error, resultadoObtenerTodos){
   if (error){
     return res.status(500).json("error en el servidor");
   }
-  res.json({ peliculas: resultadoQuery });
+    Peliculas.obtenerCantidad(req.query, function(error, resultadoObtenerCantidad){
+    if (error){
+      return res.status(500).json("error en el servidor");
+    }
+
+
+  res.json({ peliculas: resultadoObtenerTodos, total: resultadoObtenerCantidad);///poner corchete
 });
+})
 };
 
 module.exports = {
--- servidor/modelos/peliculas.js
+++ servidor/modelos/peliculas.js
@@ -11,9 +11,111 @@ var Pelicula = function(id, titulo, anio, duracion, director, fecha_lanzamiento,
   this.poster = poster;
   this.trama = trama;
 };
+// http://localhost:8080/peliculas?pagina=1&titulo=pepe&genero=4&anio=1981&cantidad=52&columna_orden=titulo&tipo_orden=ASC
+Pelicula.obtenerCantidad = function(filtros, cb){
+  var titulo = filtros.titulo;
+  var genero = filtros.genero;
+  var anio = filtros.anio;
 
-Pelicula.obtenerTodos = function(cb){
-  var consultaSql = "SELECT * from pelicula";
+  var pagina = filtros.pagina;
+  var cantidad = filtros.cantidad;
+  var columna_orden = filtros.columna_orden;
+  var tipo_orden = filtros.tipo_orden;
+
+  var contador = 0;
+  var consultaSql = "SELECT COUNT(pelicula.id) from pelicula INNER JOIN genero ON pelicula.genero_id = genero.id";
+  // condiciones
+  if (titulo) {
+    if (contador == 0) {
+      consultaSql += " WHERE";
+    } else {
+      consultaSql += " AND";
+    };
+    contador ++;
+    consultaSql += " pelicula.titulo LIKE '" + titulo + "%'";
+  };
+//select * from pelicula join  where ;
+
+  if (genero) {
+    if (contador == 0) {
+      consultaSql += " WHERE";
+    } else {
+      consultaSql += " AND";
+    };
+    contador ++;
+    consultaSql += " pelicula.genero_id = '" + genero + "'";
+  };
+
+  if (anio) {
+    if (contador == 0) {
+      consultaSql += " WHERE";
+    } else {
+      consultaSql += " AND";
+    };
+    contador ++;
+    consultaSql += " pelicula.anio = " + anio;
+  };
+
+
+  consultaSql += ";";
+  console.log(consultaSql);
+  conexion.query(consultaSql, cb);
+
+};
+
+Pelicula.obtenerTodos = function(filtros, cb){
+  var titulo = filtros.titulo;
+  var genero = filtros.genero;
+  var anio = filtros.anio;
+
+  var pagina = filtros.pagina;
+  var cantidad = filtros.cantidad;
+  var columna_orden = filtros.columna_orden;
+  var tipo_orden = filtros.tipo_orden;
+
+  var contador = 0;
+  var consultaSql = "SELECT * from pelicula INNER JOIN genero ON pelicula.genero_id = genero.id";
+  // condiciones
+  if (titulo) {
+    if (contador == 0) {
+      consultaSql += " WHERE";
+    } else {
+      consultaSql += " AND";
+    };
+    contador ++;
+    consultaSql += " pelicula.titulo LIKE '" + titulo + "%'";
+  };
+//select * from pelicula join  where ;
+
+  if (genero) {
+    if (contador == 0) {
+      consultaSql += " WHERE";
+    } else {
+      consultaSql += " AND";
+    };
+    contador ++;
+    consultaSql += " pelicula.genero_id = '" + genero + "'";
+  };
+
+  if (anio) {
+    if (contador == 0) {
+      consultaSql += " WHERE";
+    } else {
+      consultaSql += " AND";
+    };
+    contador ++;
+    consultaSql += " pelicula.anio = " + anio;
+  };
+
+  // orden
+  consultaSql += " ORDER BY " + columna_orden;
+
+  // paginacion
+  consultaSql += ` LIMIT ${(pagina - 1) * cantidad},${cantidad}`;
+
+
+  consultaSql += ";";
+  console.log(consultaSql);
   conexion.query(consultaSql, cb);
 };
 
